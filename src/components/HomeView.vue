9867f4d65b40ddb6deb1033a08f60fd5299e0a74
ouhoy <kimkinon@gmail.com>
2023-12-09 00:01:27 +0100

    finish mobile view header

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,21 +3,35 @@
 </script>
 
 <template>
-<div class="container">
-<header>
-  <img src="src/assets/images/tobelist friend.svg" alt="tobelist friend" srcset="">
-  <div>
-    <div class="header-titles">
-      <h1>Forget your todolist<br>Create your tobelist</h1>
-      <button class="primary-btn ">Create my tobelist -free</button>
-      <p>Tobelist is for boosting productivity. Unlike many other tools out there, this one can incorporate multiple methods.</p>
-    </div>
-    <div>
+  <div class="container">
+    <header>
+      <img src="src/assets/images/tobelist friend.svg" alt="tobelist friend" srcset="">
+      <div>
+        <div class="header-titles">
+          <h1>Forget your todolist<br>Create your tobelist</h1>
+          <button class="primary-btn ">Create my tobelist -free</button>
+          <p>Tobelist is for boosting productivity. Unlike many other tools out there, this one can incorporate many
+            multiple methods.</p>
+          <div class="numbers">
+            <div class="number-container">
+              <h2>030+</h2>
+              <p>users trust us</p>
+            </div>
+            <div class="number-container">
+              <h2>900+</h2>
+              <p>tasks done</p>
+            </div>
+            <div class="number-container">
+              <h2>100%</h2>
+              <p>open source</p>
+            </div>
+          </div>
+        </div>
 
-    </div>
+
+      </div>
+    </header>
   </div>
-</header>
-</div>
 </template>
 
 <style lang="scss">
