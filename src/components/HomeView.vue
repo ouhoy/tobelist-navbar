2746b797db0567f84ee9ee926f44821788582075
ouhoy <kimkinon@gmail.com>
2023-12-12 07:50:39 +0100

    make minor header ui chnages

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -5,13 +5,16 @@
 <template>
   <div class="container">
     <header>
+    <div class="img-container">
       <img src="src/assets/images/tobelist friend.svg" alt="tobelist friend" srcset="">
+
+    </div>
       <div>
         <div class="header-titles">
           <h1>Forget your Todolist<br>Create your Tobelist</h1>
-          <button class="primary-btn ">Create my tobelist -free</button>
           <p>Tobelist is for boosting productivity. Unlike many other tools out there, this one can incorporate many
             multiple methods.</p>
+          <button class="primary-btn ">Create my tobelist -free</button>
           <div class="numbers">
             <div class="number-container">
               <h2>030+</h2>
@@ -32,6 +35,12 @@
       </div>
     </header>
   </div>
+  <main>
+    <section class="explore">
+
+      
+    </section>
+  </main>
 </template>
 
 <style lang="scss">
